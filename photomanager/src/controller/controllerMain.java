23a4df8acee44735ce497117f624d758a766e157
EZambranoFx <86860458+EZambranoFx@users.noreply.github.com>
2022-06-23 01:21:52 -0500

    Implementacion Boton Buscar

--- photomanager/src/controller/controllerMain.java
+++ photomanager/src/controller/controllerMain.java
@@ -6,7 +6,16 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeView;
+import javafx.scene.input.MouseEvent;
+import model.Foto;
 
 /**
  * FXML Controller class
@@ -15,6 +24,20 @@ import javafx.fxml.Initializable;
  */
 public class controllerMain implements Initializable {
 
+    @FXML
+    private Button BuscarFoto;
+    @FXML
+    private TextField TFLugar;
+    @FXML
+    private TextField TFPersona;
+    @FXML
+    private CheckBox CBPorLugar;
+    @FXML
+    private CheckBox CBPorPersona;
+    @FXML
+    private TreeView<Foto> TVRoot;
+
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +45,19 @@ public class controllerMain implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void BuscarInTF(MouseEvent event) {
+        String Lugar=TFLugar.getText();
+        String Persona=TFPersona.getText();
+        if(!(CBPorPersona.isSelected() && CBPorLugar.isSelected())){
+            TVRoot.setRoot(ti);//TRAER METODO QUE CARGE EL ARRAYLIST DE TODAS LAS FOTOS
+            TreeTableColumn<Foto,String>columna=new TreeTableColumn<>("Folder");
+            columna.setPrefWidth(150);
+            columna.setCellValueFactory((TreeTableColumn.CellDataFeatures<Foto,String>parametro)->
+                    new ReadOnlyStringWrapper()parametro.getValue().getAtributodelaFoto())//implementar metodo
+            );
+        }
+    }
     
 }
